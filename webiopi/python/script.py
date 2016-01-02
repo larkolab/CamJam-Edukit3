155e646942cc47112cf58e1bad91f844d9d6a608
larko <larkolab@gmail.com>
2016-01-03 00:20:25 +0100

    Added webiopi script for browser control

---
+++ webiopi/python/script.py
@@ -0,0 +1,102 @@
+# This version uses new-style automatic setup/destroy/mapping
+# Need to change /etc/webiopi
+
+# Imports
+import webiopi
+
+# Retrieve GPIO lib
+GPIO = webiopi.GPIO
+
+# -------------------------------------------------- #
+# Constants definition                               #
+# -------------------------------------------------- #
+
+# Left motor GPIOs
+L1=7
+L2=8
+
+# Right motor GPIOs
+R1=9
+R2=10
+
+# -------------------------------------------------- #
+# Left Motor Functions                               #
+# -------------------------------------------------- #
+
+def left_stop():
+    GPIO.output(L1, GPIO.LOW)
+    GPIO.output(L2, GPIO.LOW)
+
+def left_forward():
+    GPIO.output(L1, GPIO.HIGH)
+    GPIO.output(L2, GPIO.LOW)
+
+def left_backward():
+    GPIO.output(L1, GPIO.LOW)
+    GPIO.output(L2, GPIO.HIGH)
+
+# -------------------------------------------------- #
+# Right Motor Functions                              #
+# -------------------------------------------------- #
+def right_stop():
+    GPIO.output(R1, GPIO.LOW)
+    GPIO.output(R2, GPIO.LOW)
+
+def right_forward():
+    GPIO.output(R1, GPIO.HIGH)
+    GPIO.output(R2, GPIO.LOW)
+
+def right_backward():
+    GPIO.output(R1, GPIO.LOW)
+    GPIO.output(R2, GPIO.HIGH)
+
+# -------------------------------------------------- #
+# Macro definition part                              #
+# -------------------------------------------------- #
+@webiopi.macro
+def go_forward():
+    left_forward()
+    right_forward()
+
+@webiopi.macro
+def go_backward():
+    left_backward()
+    right_backward()
+
+@webiopi.macro
+def turn_left():
+    left_backward()
+    right_forward()
+
+@webiopi.macro
+def turn_right():
+    left_forward()
+    right_backward()
+
+@webiopi.macro
+def stop():
+    left_stop()
+    right_stop()
+    
+# Called by WebIOPi at script loading
+def setup():
+    # Setup GPIOs
+    GPIO.setFunction(L1, GPIO.OUT)
+    GPIO.setFunction(L2, GPIO.OUT)
+    
+    GPIO.setFunction(R1, GPIO.OUT)
+    GPIO.setFunction(R2, GPIO.OUT)
+    
+    stop()
+
+
+# Called by WebIOPi at server shutdown
+def destroy():
+    # Reset GPIO functions
+    GPIO.setFunction(L1, GPIO.IN)
+    GPIO.setFunction(L2, GPIO.IN)
+    
+    GPIO.setFunction(R1, GPIO.IN)
+    GPIO.setFunction(R2, GPIO.IN)
+    
+
