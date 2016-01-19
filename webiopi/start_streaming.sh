e02459ca614befaf4acec2a44d0b92ed3cd8ab8a
larko <larkolab@gmail.com>
2016-01-19 23:07:37 +0100

    Added start script for mjpeg-streamer

---
+++ webiopi/start_streaming.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+MJPEG_STREAMER_PATH=/home/pi/Downloads/mjpeg-streamer/mjpg-streamer/mjpg-streamer-experimental/
+
+export LD_LIBRARY_PATH=$MJPEG_STREAMER_PATH
+
+$MJPEG_STREAMER_PATH/mjpg_streamer -o "output_http.so -w $MJPEG_STREAMER_PATH/www" -i "input_raspicam.so -x 320 -y 240 -fps 25 -ex night"
